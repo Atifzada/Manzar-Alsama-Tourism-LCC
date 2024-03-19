317621b837c95274a2355e237a5ad4d642220ff8
Atif <atifkhatak242@gmail.com>
2024-03-19 18:50:23 +0500

    Initial commit from Create Next App

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className='flexBetween max-container px-12 z-30 py-2 shadow-xl'>
+      Nav
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
