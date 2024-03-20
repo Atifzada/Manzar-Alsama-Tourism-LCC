238c9bfdd9126d6f106f10955069b01dc468f9b8
Atif <atifkhatak242@gmail.com>
2024-03-20 21:04:19 +0500

    Create Hero section of the website

---
+++ constant/index.tsx
@@ -0,0 +1,38 @@
+// NAVIGATION
+
+export const NAV_LINK =[
+{ href:"", key: "home", label: "Home", iconURL:"/home.svg" },
+{ href:"", key: "about", label: "About", iconURL:"/about.svg" },
+{ href:"", key: "packages", label: "Packages", iconURL:"/earth.svg" },
+{ href:"", key: "contact", label: "Contact", iconURL:"/contact.svg" },
+];
+
+// CATEGORIES SECTION
+
+export const CATEGORIES = [
+    {
+        title: "places",
+        icon: "/earth.svg",
+    },
+    {
+        title: "restaurant",
+        icon: "/restaurant.svg",
+    },
+    {
+        title: "hotels",
+        icon: "/earth.svg",
+    },
+    {
+        title: "shopping",
+        icon: "/shopping.svg",
+    },
+];
+
+// FEATURE SECTION
+
+export const FEATURE = [
+    {
+        title: "Desert Safari",
+        URL: "/safari.svg"
+    }
+];
